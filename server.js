27e28257883ab52e1e84e95bd68b075be30257c8
Debby ermel <debbyermel@gmail.com>
2018-07-13 18:26:12 -0500

    start app

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+const api = require('./server/routes/api');
+
+//parses
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/api', api);
+
+app.get('*',(req, res) => {
+ res.sendfile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`running on localhost: ${port}`));
