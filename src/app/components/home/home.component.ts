f16ac9a4a60230b1452907991ee01dd7c254f999
Debby ermel <debbyermel@gmail.com>
2018-07-17 10:11:33 -0500

    Updated home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from './../../article.service';
-import { Article } from './../../../article';
-
+import { Article } from '../../../article';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  artcicles: Array<Article>;
+  articles: Array<Article>;
 
 
   constructor(private _articleService: ArticleService) { }
 
   ngOnInit() {
     this._articleService.getArticles()
-    .subscribe(res => this.artcicles = res);
+    .subscribe(res => this.articles = res);
   }
 }
